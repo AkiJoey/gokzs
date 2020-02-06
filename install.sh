afc4978ccb67c94e0b784e98ff42ffcac1e8268f
AkiJoey <akijoey1010635951@gmail.com>
2020-02-06 22:08:53 +0800

    default commit

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 
 yum update
 yum install -y git
-git clone https://github.com/AkiJoey/GokzServer.git "$HOME/csgo/csgo" --depth=1
+git clone https://github.com/AkiJoey/GokzServer.git /home/csgo/csgo --depth=1
+
+yum install -y nginx
+sed 's/usr\/share\/nginx\/html/home\/csgo\/csgo/g' /etc/nginx/nginx.conf
 
 echo "Plugin collection install complete!"
 
